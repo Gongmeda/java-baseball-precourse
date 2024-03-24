c022c2f02bb00ff7311b1021ec15c59cff599ce7
Gongmeda <gongmeda@gmail.com>
2024-03-24 19:35:52 +0900

    feat: 스트라이크/볼 개수 계산 로직 구현

---
+++ src/main/java/model/GameResult.java
@@ -0,0 +1,20 @@
+package model;
+
+public class GameResult {
+
+    private final int strikeCount;
+    private final int ballCount;
+
+    public GameResult(int strikeCount, int ballCount) {
+        this.strikeCount = strikeCount;
+        this.ballCount = ballCount;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+}
